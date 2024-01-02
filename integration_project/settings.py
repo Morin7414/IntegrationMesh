cdd536a92b25c18c2584abdc37e5cec380c94654
Dean Morin <morin7414@gmail.com>
2024-01-01 22:50:33 -0600

    updating static

--- integration_project/settings.py
+++ integration_project/settings.py
@@ -123,9 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_URL = 'static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Adjust the path as needed
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+ 
+]
 
 
 # Default primary key field type

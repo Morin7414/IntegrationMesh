fd9e299b051042a5f2050c0672184206e2ae7a12
Dean Morin <morin7414@gmail.com>
2024-07-09 00:45:39 -0600

    fixing it up

--- assets/views.py
+++ assets/views.py
@@ -153,7 +153,7 @@ def confirm_updates(request):
                     except IntegrityError as e:
                         print(f"Integrity Error: {e}")
                         print(traceback.format_exc())
-                        return JsonResponse({"error": f"Database integrity error: {str(e).split('\\n')[0]}"}, status=500)
+                        return JsonResponse({"error": f"Database integrity error: {str(e).split('\n')[0]}"}, status=500)
 
             request.session.pop('updates', None)
             request.session.pop('new_records', None)
